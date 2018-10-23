80c40be29878d8321160f21feddbaaadd2cd29af
John <jeg11@sunyorange.edu>
2018-10-23 17:20:35 -0400

    Finished compileFile function and excecute file function

--- applicationgui.py
+++ applicationgui.py
@@ -3,19 +3,89 @@ from tkinter import filedialog
 from os import system
 
 class PythonApplication:
+
+
     def selectFile(self, event):
-        fp1 = filedialog.askopenfilename(initialdir = '/home',
+        fp1 = filedialog.askopenfilename(initialdir = './',
                                               title = 'Select file', 
                                           filetypes = (("java files" ,"*.java"),("all files", "*.*")))
-
+        
         self.fileLocationLabel['text'] = str(fp1)
 
     def compileFile(self, event):
-        temp = self.fileLocationLabel['text']        
-        #system("find ." + )
+
+        i = 0
+        directoryList = []
+        directoryList.append(None)
+
+        for ch in self.fileLocationLabel['text']:
+            if (directoryList[0] == None or
+                directoryList[0] == ' ' or 
+                directoryList[i] == None):
+            
+                directoryList[i] = ch
+            else:
+                if(ch == '/'):
+                    directoryList.append(None)
+                    i += 1
+                else:
+                    directoryList[i] = str(directoryList[i]) + str(ch)
+     
+
+
+        directoryList[0] = directoryList[0][1:]
+        dire = ""
+        file1 = ""
+        for folder in directoryList:
+            if ('.java' in folder):
+                file1 = folder                
+                break
+            dire = dire + '/' + folder
+        print(dire)   
+        try:
+            system("cd " + dire + "&& javac " + file1)
+        except:
+            print("t")
 
     def executeFile(self, event):
-        pass
+        
+        i = 0
+        directoryList = []
+        directoryList.append(None)
+
+        for ch in self.fileLocationLabel['text']:
+            if (directoryList[0] == None or
+                directoryList[0] == ' ' or 
+                directoryList[i] == None):
+            
+                directoryList[i] = ch
+            else:
+                if(ch == '/'):
+                    directoryList.append(None)
+                    i += 1
+                else:
+                    directoryList[i] = str(directoryList[i]) + str(ch)
+     
+
+
+        directoryList[0] = directoryList[0][1:]
+        filelist = []
+        filelist.append(None)
+        dire = ""
+        file1 = ""
+        for folder in directoryList:
+            if ('.java' in folder):
+                file1 = folder
+                x = file1.split('.')
+                print(x[0])
+                file1 = x[0]
+                break
+            dire = dire + '/' + folder
+        print(dire)   
+        try:
+            system("cd " + dire + "&& java " + file1)
+        except:
+            print("t")
 
     def __init__(self, master):
         frame = Frame(master)
